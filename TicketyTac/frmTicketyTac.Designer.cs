343e2399dab2e687c1650725a8fe0120320eef74
Jay <28309688+mrjaywilson@users.noreply.github.com>
2018-12-20 10:31:21 -0800

    Add files via upload

---
+++ TicketyTac/frmTicketyTac.Designer.cs
@@ -0,0 +1,274 @@
+﻿namespace TicketyTac
+{
+    partial class frmTicketyTac
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.pbNine = new System.Windows.Forms.PictureBox();
+            this.pbSix = new System.Windows.Forms.PictureBox();
+            this.pbThree = new System.Windows.Forms.PictureBox();
+            this.pbEight = new System.Windows.Forms.PictureBox();
+            this.pbSeven = new System.Windows.Forms.PictureBox();
+            this.pbFive = new System.Windows.Forms.PictureBox();
+            this.pbFour = new System.Windows.Forms.PictureBox();
+            this.pbTwo = new System.Windows.Forms.PictureBox();
+            this.pbOne = new System.Windows.Forms.PictureBox();
+            this.pictureBox10 = new System.Windows.Forms.PictureBox();
+            this.btnNewGame = new System.Windows.Forms.Button();
+            this.btnExit = new System.Windows.Forms.Button();
+            this.lblNotice = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.pbNine)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pbSix)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pbThree)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pbEight)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pbSeven)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pbFive)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pbFour)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pbTwo)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pbOne)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox10)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pbNine
+            // 
+            this.pbNine.BackColor = System.Drawing.Color.Transparent;
+            this.pbNine.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Center;
+            this.pbNine.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.pbNine.Location = new System.Drawing.Point(397, 513);
+            this.pbNine.Margin = new System.Windows.Forms.Padding(4, 3, 4, 3);
+            this.pbNine.Name = "pbNine";
+            this.pbNine.Size = new System.Drawing.Size(150, 150);
+            this.pbNine.TabIndex = 9;
+            this.pbNine.TabStop = false;
+            // 
+            // pbSix
+            // 
+            this.pbSix.BackColor = System.Drawing.Color.Transparent;
+            this.pbSix.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Center;
+            this.pbSix.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.pbSix.Location = new System.Drawing.Point(397, 334);
+            this.pbSix.Margin = new System.Windows.Forms.Padding(4, 3, 4, 3);
+            this.pbSix.Name = "pbSix";
+            this.pbSix.Size = new System.Drawing.Size(150, 150);
+            this.pbSix.TabIndex = 8;
+            this.pbSix.TabStop = false;
+            // 
+            // pbThree
+            // 
+            this.pbThree.BackColor = System.Drawing.Color.Transparent;
+            this.pbThree.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Center;
+            this.pbThree.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.pbThree.Location = new System.Drawing.Point(397, 154);
+            this.pbThree.Margin = new System.Windows.Forms.Padding(4, 3, 4, 3);
+            this.pbThree.Name = "pbThree";
+            this.pbThree.Size = new System.Drawing.Size(150, 150);
+            this.pbThree.TabIndex = 7;
+            this.pbThree.TabStop = false;
+            // 
+            // pbEight
+            // 
+            this.pbEight.BackColor = System.Drawing.Color.Transparent;
+            this.pbEight.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Center;
+            this.pbEight.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.pbEight.Location = new System.Drawing.Point(212, 513);
+            this.pbEight.Margin = new System.Windows.Forms.Padding(4, 3, 4, 3);
+            this.pbEight.Name = "pbEight";
+            this.pbEight.Size = new System.Drawing.Size(150, 150);
+            this.pbEight.TabIndex = 6;
+            this.pbEight.TabStop = false;
+            // 
+            // pbSeven
+            // 
+            this.pbSeven.BackColor = System.Drawing.Color.Transparent;
+            this.pbSeven.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Center;
+            this.pbSeven.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.pbSeven.Location = new System.Drawing.Point(29, 513);
+            this.pbSeven.Margin = new System.Windows.Forms.Padding(4, 3, 4, 3);
+            this.pbSeven.Name = "pbSeven";
+            this.pbSeven.Size = new System.Drawing.Size(150, 150);
+            this.pbSeven.TabIndex = 5;
+            this.pbSeven.TabStop = false;
+            // 
+            // pbFive
+            // 
+            this.pbFive.BackColor = System.Drawing.Color.Transparent;
+            this.pbFive.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Center;
+            this.pbFive.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.pbFive.Location = new System.Drawing.Point(212, 334);
+            this.pbFive.Margin = new System.Windows.Forms.Padding(4, 3, 4, 3);
+            this.pbFive.Name = "pbFive";
+            this.pbFive.Size = new System.Drawing.Size(150, 150);
+            this.pbFive.TabIndex = 4;
+            this.pbFive.TabStop = false;
+            // 
+            // pbFour
+            // 
+            this.pbFour.BackColor = System.Drawing.Color.Transparent;
+            this.pbFour.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Center;
+            this.pbFour.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.pbFour.Location = new System.Drawing.Point(29, 334);
+            this.pbFour.Margin = new System.Windows.Forms.Padding(4, 3, 4, 3);
+            this.pbFour.Name = "pbFour";
+            this.pbFour.Size = new System.Drawing.Size(150, 150);
+            this.pbFour.TabIndex = 3;
+            this.pbFour.TabStop = false;
+            // 
+            // pbTwo
+            // 
+            this.pbTwo.BackColor = System.Drawing.Color.Transparent;
+            this.pbTwo.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Center;
+            this.pbTwo.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.pbTwo.Location = new System.Drawing.Point(212, 154);
+            this.pbTwo.Margin = new System.Windows.Forms.Padding(4, 3, 4, 3);
+            this.pbTwo.Name = "pbTwo";
+            this.pbTwo.Size = new System.Drawing.Size(150, 150);
+            this.pbTwo.TabIndex = 2;
+            this.pbTwo.TabStop = false;
+            // 
+            // pbOne
+            // 
+            this.pbOne.BackColor = System.Drawing.Color.Transparent;
+            this.pbOne.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Center;
+            this.pbOne.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.pbOne.Location = new System.Drawing.Point(29, 154);
+            this.pbOne.Margin = new System.Windows.Forms.Padding(4, 3, 4, 3);
+            this.pbOne.Name = "pbOne";
+            this.pbOne.Size = new System.Drawing.Size(150, 150);
+            this.pbOne.TabIndex = 1;
+            this.pbOne.TabStop = false;
+            // 
+            // pictureBox10
+            // 
+            this.pictureBox10.BackColor = System.Drawing.Color.Black;
+            this.pictureBox10.BackgroundImage = global::TicketyTac.Properties.Resources.title_one;
+            this.pictureBox10.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.pictureBox10.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.pictureBox10.Location = new System.Drawing.Point(29, 24);
+            this.pictureBox10.Margin = new System.Windows.Forms.Padding(4, 3, 4, 3);
+            this.pictureBox10.Name = "pictureBox10";
+            this.pictureBox10.Size = new System.Drawing.Size(333, 100);
+            this.pictureBox10.TabIndex = 10;
+            this.pictureBox10.TabStop = false;
+            // 
+            // btnNewGame
+            // 
+            this.btnNewGame.FlatAppearance.BorderColor = System.Drawing.Color.Silver;
+            this.btnNewGame.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnNewGame.Font = new System.Drawing.Font("MS Reference Sans Serif", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnNewGame.ForeColor = System.Drawing.SystemColors.AppWorkspace;
+            this.btnNewGame.Location = new System.Drawing.Point(370, 24);
+            this.btnNewGame.Margin = new System.Windows.Forms.Padding(4, 3, 4, 3);
+            this.btnNewGame.Name = "btnNewGame";
+            this.btnNewGame.Size = new System.Drawing.Size(156, 38);
+            this.btnNewGame.TabIndex = 11;
+            this.btnNewGame.Text = "NEW GAME";
+            this.btnNewGame.UseVisualStyleBackColor = true;
+            this.btnNewGame.Click += new System.EventHandler(this.btnNewGame_Click);
+            // 
+            // btnExit
+            // 
+            this.btnExit.FlatAppearance.BorderColor = System.Drawing.Color.Silver;
+            this.btnExit.FlatAppearance.BorderSize = 0;
+            this.btnExit.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnExit.Font = new System.Drawing.Font("MS Reference Sans Serif", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnExit.ForeColor = System.Drawing.SystemColors.AppWorkspace;
+            this.btnExit.Location = new System.Drawing.Point(534, 24);
+            this.btnExit.Margin = new System.Windows.Forms.Padding(4, 3, 4, 3);
+            this.btnExit.Name = "btnExit";
+            this.btnExit.Size = new System.Drawing.Size(29, 36);
+            this.btnExit.TabIndex = 12;
+            this.btnExit.Text = "X";
+            this.btnExit.UseVisualStyleBackColor = true;
+            this.btnExit.Click += new System.EventHandler(this.btnExit_Click);
+            // 
+            // lblNotice
+            // 
+            this.lblNotice.Font = new System.Drawing.Font("Impact", 15.75F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblNotice.ForeColor = System.Drawing.Color.IndianRed;
+            this.lblNotice.Location = new System.Drawing.Point(370, 74);
+            this.lblNotice.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.lblNotice.Name = "lblNotice";
+            this.lblNotice.Size = new System.Drawing.Size(177, 63);
+            this.lblNotice.TabIndex = 13;
+            this.lblNotice.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // frmTicketyTac
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Black;
+            this.ClientSize = new System.Drawing.Size(576, 693);
+            this.Controls.Add(this.lblNotice);
+            this.Controls.Add(this.btnExit);
+            this.Controls.Add(this.btnNewGame);
+            this.Controls.Add(this.pbNine);
+            this.Controls.Add(this.pbSix);
+            this.Controls.Add(this.pbThree);
+            this.Controls.Add(this.pbEight);
+            this.Controls.Add(this.pbSeven);
+            this.Controls.Add(this.pbFive);
+            this.Controls.Add(this.pbFour);
+            this.Controls.Add(this.pbTwo);
+            this.Controls.Add(this.pbOne);
+            this.Controls.Add(this.pictureBox10);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.Name = "frmTicketyTac";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Form1";
+            this.TransparencyKey = System.Drawing.Color.White;
+            ((System.ComponentModel.ISupportInitialize)(this.pbNine)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pbSix)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pbThree)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pbEight)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pbSeven)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pbFive)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pbFour)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pbTwo)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pbOne)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox10)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+        private System.Windows.Forms.PictureBox pbOne;
+        private System.Windows.Forms.PictureBox pbTwo;
+        private System.Windows.Forms.PictureBox pbFour;
+        private System.Windows.Forms.PictureBox pbFive;
+        private System.Windows.Forms.PictureBox pbSeven;
+        private System.Windows.Forms.PictureBox pbEight;
+        private System.Windows.Forms.PictureBox pbThree;
+        private System.Windows.Forms.PictureBox pbSix;
+        private System.Windows.Forms.PictureBox pbNine;
+        private System.Windows.Forms.PictureBox pictureBox10;
+        private System.Windows.Forms.Button btnNewGame;
+        private System.Windows.Forms.Button btnExit;
+        private System.Windows.Forms.Label lblNotice;
+    }
+}
+
---
+++ TicketyTac/frmTicketyTac.cs
@@ -0,0 +1,336 @@
+﻿// Namespace inclusion
+using System;
+using System.Drawing;
+using System.Windows.Forms;
+
+/*
+ *      Program:        Project 4
+ *      File:           frmTicketyTac.cs
+ *      Description:    Handles everything related for the game.
+ *      Author:         Jay Wilson
+ *      Class:          CST-117
+ *      Date:           July 1, 2018
+ */
+
+/// <summary>
+/// Declare namespace
+/// </summary>
+namespace TicketyTac
+{
+    /// <summary>
+    /// Class declaration
+    /// </summary>
+    public partial class frmTicketyTac : Form
+    {
+        /// <summary>
+        /// Declare and initialize variables
+        /// </summary>
+        private char currentMove = 'X';         // X is player and goes first
+        private char[,] cell = new char[3, 3];  // Setup the array for hanlding the game data
+
+        /// <summary>
+        /// Initialization method
+        /// </summary>
+        public frmTicketyTac()
+        {
+            // Initialize form components
+            InitializeComponent();
+
+            // Setup click events
+            pbOne.Click += Player_Click;
+            pbTwo.Click += Player_Click;
+            pbThree.Click += Player_Click;
+            pbFour.Click += Player_Click;
+            pbFive.Click += Player_Click;
+            pbSix.Click += Player_Click;
+            pbSeven.Click += Player_Click;
+            pbEight.Click += Player_Click;
+            pbNine.Click += Player_Click;
+
+            // Initial reset of the game to ensure everything
+            // is set to be ready for a new game.
+            ResetGame();
+
+        }
+
+        /// <summary>
+        /// Event to handle all the player clicks
+        /// </summary>
+        private void Player_Click(object sender, EventArgs e)
+        {
+            // Get the control name and store it
+            string name = ((PictureBox)sender).Name;
+
+            // choose action depending on control name
+            // and call setMove()
+            switch (name)
+            {
+                case "pbOne":
+                    SetMove(0, 0, pbOne);
+                    break;
+                case "pbTwo":
+                    SetMove(0, 1, pbTwo);
+                    break;
+                case "pbThree":
+                    SetMove(0, 2, pbThree);
+                    break;
+                case "pbFour":
+                    SetMove(1, 0, pbFour);
+                    break;
+                case "pbFive":
+                    SetMove(1, 1, pbFive);
+                    break;
+                case "pbSix":
+                    SetMove(1, 2, pbSix);
+                    break;
+                case "pbSeven":
+                    SetMove(2, 0, pbSeven);
+                    break;
+                case "pbEight":
+                    SetMove(2, 1, pbEight);
+                    break;
+                case "pbNine":
+                    SetMove(2, 2, pbNine);
+                    break;
+            }
+        }
+
+        /// <summary>
+        /// Method to set the player peice in the correct location
+        /// and handle all standard game logic
+        /// </summary>
+        /// <param name="x">X location of click.</param>
+        /// <param name="y">Y location of click.</param>
+        /// <param name="pb">Which picturebox to effect.</param>
+        private void SetMove(int x, int y, PictureBox pb)
+        {
+            // Check if the cell is blank.
+            if (cell[x, y] == ' ')
+            {
+                // If so and the current player is X
+                if (currentMove.Equals('X'))
+                {
+                    // Set the image.
+                    pb.Image = Properties.Resources.x;
+
+                    // Set the cell value in the data array
+                    cell[x, y] = 'X';
+
+                    // Set the move to player 2 and notify
+                    // the players
+                    currentMove = 'Y';
+                    lblNotice.ForeColor = Color.CornflowerBlue;
+                    lblNotice.Text = "PLAYER O\nGO!";
+
+                    // Check for winner
+                    if (CheckWinner('X'))
+                    {
+                        // Let players know who won
+                        lblNotice.ForeColor = Color.IndianRed;
+                        lblNotice.Text = "Player X wins!";
+                        GameStatus(false);
+                    }
+                    else
+                    {
+                        // If the gameboard is full and no winner
+                        if (IsFull())
+                        {
+                            // Notify Players of draw
+                            lblNotice.ForeColor = Color.Yellow;
+                            lblNotice.Text = "Draw!";
+                            GameStatus(false);
+                        }
+                    }
+                }
+                else
+                {
+                    // Set the image.
+                    pb.Image = Properties.Resources.o;
+
+                    // Set the  cell value in the data array
+                    cell[x, y] = 'O';
+
+                    // Set the move to player 1
+                    currentMove = 'X';
+                    lblNotice.ForeColor = Color.IndianRed;
+                    lblNotice.Text = "PLAYER X\nGO";
+
+                    // Check for winner
+                    if (CheckWinner('O'))
+                    {
+                        // Let players know who won
+                        lblNotice.ForeColor = Color.CornflowerBlue;
+                        lblNotice.Text = "Player O wins!";
+                        GameStatus(false);
+                    }
+                    else
+                    {
+                        // If the gameboard is full and no winner
+                        if (IsFull())
+                        {
+                            // Notify Players of draw
+                            lblNotice.ForeColor = Color.Yellow;
+                            lblNotice.Text = "Draw!";
+                            GameStatus(false);
+                        }
+                    }
+                }
+            }
+        }
+
+        /// <summary>
+        /// Method to handle the enable / disable the picture boxes
+        /// </summary>
+        /// <param name="status">Boolean value for enabling / disabling gameboard.</param>
+        private void GameStatus(bool status)
+        {
+            // Sets the status of the gameboard
+            pbOne.Enabled = status;
+            pbTwo.Enabled = status;
+            pbThree.Enabled = status;
+            pbFour.Enabled = status;
+            pbFive.Enabled = status;
+            pbSix.Enabled = status;
+            pbSeven.Enabled = status;
+            pbEight.Enabled = status;
+            pbNine.Enabled = status;
+        }
+
+        /// <summary>
+        /// Check if the gameboard is full
+        /// </summary>
+        private bool IsFull()
+        {
+            // Iterate through the data for ' '
+            for (int i = 0; i < 3; i++)
+            {
+                for (int j = 0; j < 3; j++)
+                {
+                    // If any cell is empty ' '...
+                    if (cell[i,j] == ' ')
+                    {
+                        return false;
+                    }
+                }
+            }
+            // Otherwise the gameboard is full
+            return true;
+        }
+
+        /// <summary>
+        /// Method to check for winner
+        /// </summary>
+        /// <param name="player">set the tokn for the player, either 'X' or 'O'</param>
+        private bool CheckWinner(char player)
+        {
+            // Interate through to find triple tokens
+            for (int i = 0; i < 3; i++)
+            {
+                // Check the three cell values
+                if (cell[i, 0] == player &&
+                    cell[i, 1] == player &&
+                    cell[i, 2] == player)
+                {
+                    // If they all equal the same token value
+                    return true;
+                }
+            }
+
+            // Interate through to find triple tokens
+            for (int j = 0; j < 3; j++)
+            {
+                // Check the three cell values
+                if (cell[0, j] == player &&
+                    cell[1, j] == player &&
+                    cell[2, j] == player)
+                {
+                    // If they all equal the same token value
+                    return true;
+                }
+            }
+
+            // Check cell locations that represent the diagonal locations
+            if (cell[0, 0] == player &&
+                cell[1, 1] == player &&
+                cell[2, 2] == player)
+            {
+                return true;
+            }
+
+            // Check cell locations that represent the other diagonal locations
+            if (cell[0, 2] == player &&
+                cell[1, 1] == player &&
+                cell[2, 0] == player)
+            {
+                return true;
+            }
+
+            // If all fail, no winner
+            return false;
+        }
+
+        /// <summary>
+        /// Reset the game
+        /// </summary>
+        public void ResetGame()
+        {
+            /// Iterate through all cell locations and clear them
+            for (int i = 0; i < 3; i++)
+            {
+                for (int j = 0; j < 3; j++)
+                {
+                    // in the given location, clear it
+                    cell[i, j] = ' ';
+                }
+            }
+
+            // Enable the gameboard
+            GameStatus(true);
+
+            // Clear all 'X' and 'O' images
+            pbOne.Image = null;
+            pbTwo.Image = null;
+            pbThree.Image = null;
+            pbFour.Image = null;
+            pbFive.Image = null;
+            pbSix.Image = null;
+            pbSeven.Image = null;
+            pbEight.Image = null;
+            pbNine.Image = null;
+
+            // Reset all background on the gameboard
+            pbOne.BackColor = Color.Black;
+            pbTwo.BackColor = Color.Black;
+            pbThree.BackColor = Color.Black;
+            pbFour.BackColor = Color.Black;
+            pbFive.BackColor = Color.Black;
+            pbSix.BackColor = Color.Black;
+            pbSeven.BackColor = Color.Black;
+            pbEight.BackColor = Color.Black;
+            pbNine.BackColor = Color.Black;
+
+            // Set the notice
+            currentMove = 'X';
+            lblNotice.ForeColor = Color.IndianRed;
+            lblNotice.Text = "PLAYER X\nGO";
+        }
+
+        /// <summary>
+        /// Button to exit the program
+        /// </summary>
+        private void btnExit_Click(object sender, EventArgs e)
+        {
+            // Exit the application
+            Application.Exit();
+        }
+
+        /// <summary>
+        /// Butto nto start a new game.
+        /// </summary>
+        private void btnNewGame_Click(object sender, EventArgs e)
+        {
+            // Reset the game
+            ResetGame();
+        }
+    }
+}
